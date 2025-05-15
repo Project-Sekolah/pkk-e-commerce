d73a5bfa9d2b89b624acffda4ca96c2fd9c472b0
rajendrazero <rajendraathallahfawwaz08@gmail.com>
2025-05-15 21:30:24 +0700

    finishing

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 require_once '../app/init.php';
 
-
-file_put_contents('/tmp/debug.log', "Reached index.php\n", FILE_APPEND);
-error_log("Isi /var/www/html: " . implode(", ", scandir('/var/www/html')));
 $app = new App;
 
