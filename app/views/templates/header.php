75f001799347c340a074692a7d03e29f0701db7b
rajendrazero <122064592+rajendrazero@users.noreply.github.com>
2025-09-12 11:34:36 +0700

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -14,6 +14,13 @@
   <meta name="keywords" content="lunerburg, e-commerce, produk, checkout, login, register, FAQ, pengujian, belanja online">
   <meta name="author" content="Tim Pengembang Lunerburg & Co">
 
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P7BL3D5J');</script>
+<!-- End Google Tag Manager -->
   
   <!-- Google tag (gtag.js) -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-W44SYM7XQM"></script>
@@ -26,13 +33,7 @@
   </script>
   <!-- End Google tag -->
 
-  <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P7BL3D5J');</script>
-<!-- End Google Tag Manager -->
+
 
   <!-- Open Graph (sosial media) -->
   <meta property="og:title" content="Lunerburg & Co - Toko Online Terpercaya">
