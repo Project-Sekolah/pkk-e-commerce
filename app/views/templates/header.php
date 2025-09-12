a655fe1f73813582e0d8ad32932cf18a16ebaf1b
rajendrazero <122064592+rajendrazero@users.noreply.github.com>
2025-09-12 12:00:21 +0700

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">  
 <head>  
+  
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P7BL3D5J');</script>
+<!-- End Google Tag Manager -->
+  
   <!-- Charset & Viewport -->
   <meta charset="utf-8">  
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -14,13 +23,7 @@
   <meta name="keywords" content="lunerburg, e-commerce, produk, checkout, login, register, FAQ, pengujian, belanja online">
   <meta name="author" content="Tim Pengembang Lunerburg & Co">
 
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P7BL3D5J');</script>
-<!-- End Google Tag Manager -->
+
   
   <!-- Google tag (gtag.js) -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-W44SYM7XQM"></script>
